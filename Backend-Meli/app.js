b83018e2415e3fcf6330cfb39a809aaef377bb4e
javut <jaider.vergara@globalbank.com.pa>
2022-10-24 21:15:27 -0500

    add apis backend

--- Backend-Meli/app.js
+++ Backend-Meli/app.js
@@ -1,13 +1,18 @@
-const express = require('express')
+const express = require('express');
+const fetch = (url) => 
+    import('node-fetch').then(({default: fetch}) => fetch(url));
+// import express from 'express';
+
+// import fetch from 'node-fetch';
 const app = express()
 const port = 3000
 
 
 let BD = {
-"author": {
+author: {
 "name": "Jaider",
 "lastname": "Vergara" },
-categories: ["Cat1", "Cat2", "Cat3"],
+categories: [],
 items: [
     {
         "id": "PROD001",
@@ -19,103 +24,102 @@ items: [
         },
         "picture": "http://",
         "condition": "New",
-        "free_shipping": true,
-        "sold_quantity": 1,
+        "free_shipping": false,
+        "sold_quantity": 0,
         "description": "Celular smartphone"
-
     },
     {
         "id": "PROD002",
         "title": "iPHONE 12 Apple",
-        "price": {
+         "price": {
             "currency": "AR",
             "amount": 1234,
             "decimals": 4412
         },
         "picture": "http://",
         "condition": "New",
-        "free_shipping": true,
-        "sold_quantity": 1,
+        "free_shipping": false,
+        "sold_quantity": 0,
         "description": "Celular smartphone"
-
     },
     {
         "id": "PROD003",
         "title": "Macbook Pro 13",
-        "price": {
+         "price": {
             "currency": "AR",
             "amount": 124334,
             "decimals": 44112
         },
         "picture": "http://",
         "condition": "New",
-        "free_shipping": true,
-        "sold_quantity": 1,
-        "description": "netbook"
-
+        "free_shipping": false,
+        "sold_quantity": 0,
+        "description": "Celular smartphone"
     },
     {
         "id": "PROD004",
         "title": "Samsung Galaxy 13",
-        "price": {
+         "price": {
             "currency": "AR",
             "amount": 41234,
             "decimals": 412
         },
         "picture": "http://",
         "condition": "New",
-        "free_shipping": true,
-        "sold_quantity": 1,
+        "free_shipping": false,
+        "sold_quantity": 0,
         "description": "Celular smartphone"
-
-    },
-    {
-        "id": "PROD005",
-        "title": "Xiaomi 10one",
-        "price": {
-            "currency": "AR",
-            "amount": 51234,
-            "decimals": 4412
-        },
-        "picture": "http://",
-        "condition": "New",
-        "free_shipping": true,
-        "sold_quantity": 1,
-        "description": "Celular smartphone"
-
-    },
-    {
-        "id": "PROD006",
-        "title": "Lenovo",
-        "price": {
-            "currency": "AR",
-            "amount": 551234,
-            "decimals": 4412
-        },
-        "picture": "http://",
-        "condition": "New",
-        "free_shipping": true,
-        "sold_quantity": 1,
-        "description": "Netbook"
-
     }
     
     ]
 }
 
 
+fetch("https://api.mercadolibre.com/sites/MLA/search?q=celular")
+    .then((respuesta) => {
+        return respuesta.json()
+    }).then((resp) => {
+            BD.categories.push(resp.filters[0].values[0].path_from_root[0].name);
+            for(let i = 0; i < 4 ; i++) {
+                BD.items[i].id = resp.results[i].id;
+                BD.items[i].title = resp.results[i].title;
+                BD.items[i].price.currency = resp.results[i].prices.prices[0].currency_id;
+                BD.items[i].price.amount = resp.results[i].prices.prices[0].amount;
+                BD.items[i].price.decimals = resp.results[i].prices.prices[0].amount;
+                BD.items[i].picture = resp.results[i].thumbnail;
+                BD.items[i].condition = resp.results[i].condition;
+                BD.items[i].free_shipping = resp.results[i].shipping.free_shipping;
+                BD.items[i].sold_quantity = resp.results[i].sold_quantity;
+
+                
+            }
+    })
+
+    fetch("https://api.mercadolibre.com/items/MLA916798816/description")
+    .then((respuesta) => {
+        return respuesta.json()
+    }).then((resp) => {
+        for(let i = 0; i < 4 ; i++) {
+            BD.items[i].description = resp.plain_text      
+        }
+
+    })
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  console.log(BD);
 })
+
 var result1="1";
 app.get('/api/items', (req,res)  => {
-    let query = req.param('q').toLowerCase()
+    console.log("Este es mi Query params: "+ req.query.q);
+    let query = req.query.q
     res.set('content-type', 'application/json');
-    let salida = {"author":BD.author,"items":[],categories:BD.categories};
+    let salida = {"author":BD.author,categories:BD.categories,"items":[]};
     for(let i = 0; i<BD.items.length;i++){
-    
-        if(BD.items[i].title.toLowerCase().indexOf(query)>=0 || BD.items[i].description.toLowerCase().indexOf(query)>=0){
+        if(BD.items[i].title.toLowerCase().indexOf(query)<0){
             console.log("lo encontro")
             salida['items'].push({"id":BD.items[i].id,"title":BD.items[i].title,"price":BD.items[i].price,"picture":BD.items[i].picture,"condtion":BD.items[i].condition,"free_shipping":BD.items[i].free_shipping})
         }
@@ -133,10 +137,20 @@ const getProduct = (_id) => {
     }
     return(salida)
 }
+
 app.get('/api/items/:id', (req,res)  => {
     res.set('content-type', 'application/json');
-    res.send(getProduct(req.params.id))
+    for(let x = 0;x<BD.items.length;x++){
+        console.log("Aqui entre en el api/items")
+        if(BD.items[x].id == req.params.id){
+            console.log("Hola soy igual a: " + req.params.id)
+            res.send(getProduct(req.params.id))
+        }else{
+            res.send("No se ha podido encontrar el ID")
+        }
+    }
 })
+
 app.get('/api/items/:id/description', (req,res)  => {
     res.set('content-type', 'application/json');
     res.send(getProduct(req.params.id))
