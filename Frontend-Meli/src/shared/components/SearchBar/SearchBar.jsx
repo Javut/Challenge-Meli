19b6e84c3059639e655bff9f278abcf2d8958d21
javut <jaider.vergara@globalbank.com.pa>
2022-10-26 23:56:13 -0500

    Add search for products

--- Frontend-Meli/src/shared/components/SearchBar/SearchBar.jsx
+++ Frontend-Meli/src/shared/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './SearchBar.module.scss';
-import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo_ml.png';
 import search from '../../../assets/search_2x.png';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import {useItemListContext} from '../../../context/ItemListContext'
 
-const Search = () => {
+
+const Search =  () => {
+
+    const {item, modifyItems} = useItemListContext();
+    const [value, setValue] = useState("celular");
+    const navigate = useNavigate();
+   
+
+    const handleSubmit =  (e) => {
+        try{
+
+            axios.get(`http://localhost:3000/api/items/?q=${value}`)
+            .then((response) => response)
+            .then(resp => {
+                console.log(resp);
+                modifyItems(resp.data.categories[0],resp.data.items);
+                console.log("Aqui entre y mi valor es: "+resp.data.author.name)
+            })
+            console.log("Primero navego");
+
+           
+
+        }catch(error){ 
+            console.log("Se origino el siguiente error: "+ error)
+        }
+
+        navigate('/items/search');
+      
+    }  
+
+    const onChangeHandler = (e) => {
+        setValue(e.target.value);
+        console.log(value);
+    }
+
+
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
@@ -15,10 +52,14 @@ const Search = () => {
                 <form className={styles.search_form}>
                     <input
                         type="text"
+                        id="searchinput"
                         className={styles.input}
                         placeholder="Nunca dejes de buscar"
+                        onChange={onChangeHandler}
+
                     />
-                    <button type='submit' className={styles.submit}>
+
+                    <button type='button' className={styles.submit} onClick={handleSubmit}>
                         <img src={search} alt="Search" />
                     </button>
                 </form>
--- Frontend-Meli/src/shared/pages/SearchPage/SearchPage.jsx
+++ Frontend-Meli/src/shared/pages/SearchPage/SearchPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styles from './SearchPage.module.scss';
 import Search from '../../components/SearchBar/SearchBar'
+import {useItemListContext} from '../../../context/ItemListContext'
 
 const SearchPage = () => {
+
   return (
     <div>
         <Search/>
